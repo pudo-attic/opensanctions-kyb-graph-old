e0773c1a074779ade2be378c2f6fe268c521ecf3
Friedrich Lindenberg <friedrich@pudo.org>
2022-12-12 13:10:46 +0100

    Collection in graph compiler

--- common/catalog.py
+++ common/catalog.py
@@ -1,5 +1,7 @@
 import json
+import yaml
 import requests
+from typing import Optional
 from datetime import datetime
 from nomenklatura.dataset import DataCatalog
 from nomenklatura.util import PathLike, datetime_iso
@@ -7,21 +9,24 @@ from zavod.dataset import ZavodDataset
 
 
 def build_catalog(catalog_in: PathLike):
+    with open(catalog_in, "r") as fh:
+        catalog_in_data = yaml.safe_load(fh)
     catalog = DataCatalog(ZavodDataset, {})
     catalog.updated_at = datetime_iso(datetime.utcnow())
-    with open(catalog_in, "r") as fh:
-        while line := fh.readline():
-            url = line.strip()
+    for ds_data in catalog_in_data["datasets"]:
+        include_url: Optional[str] = ds_data.pop("include", None)
+        if include_url is not None:
             try:
-                resp = requests.get(url)
-                data = resp.json()
-                catalog.make_dataset(data)
+                resp = requests.get(include_url)
+                ds_data = resp.json()
             except Exception as exc:
-                print("ERROR [%s]: %s" % (url, exc))
+                print("ERROR [%s]: %s" % (include_url, exc))
+        ds = catalog.make_dataset(ds_data)
+        print("Dataset: %r" % ds)
 
     with open("catalog.json", "w") as fh:
         json.dump(catalog.to_dict(), fh)
 
 
 if __name__ == "__main__":
-    build_catalog("catalog.txt")
+    build_catalog("catalog.in.yml")
