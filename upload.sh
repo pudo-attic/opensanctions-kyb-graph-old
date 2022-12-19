44f4ac3662fb1f71d4509664af51f8303498fa04
Friedrich Lindenberg <friedrich@pudo.org>
2022-12-19 17:21:54 +0100

    fix cache invalidation

--- upload.sh
+++ upload.sh
@@ -7,4 +7,5 @@
 # example:
 #   bash ../../scripts/upload.sh icij_offshoreleaks data/export
 
-aws s3 sync --no-progress --cache-control "public, max-age=64600" --metadata-directive REPLACE --acl public-read $2 s3://data.opensanctions.org/graph/$1
+aws s3 sync --no-progress --content-disposition attachment --metadata-directive REPLACE --acl public-read $2 s3://data.opensanctions.org/graph/$1
+aws cloudfront create-invalidation --distribution-id ETROMAQBEJS91 --paths "/graph/$1/*"
